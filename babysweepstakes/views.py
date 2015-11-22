30fc158d4d03043cb69f857d5f505938ae10b7a6
Ben Banfield-Zanin <ben@banfieldzanin.com>
2015-11-22 17:52:06 +0000

    Tidy up views

--- babysweepstakes/views.py
+++ babysweepstakes/views.py
@@ -1,32 +1,34 @@
 from pyramid.httpexceptions import (
     HTTPFound,
     )
-from pyramid.view import view_config
+from pyramid.view import view_config, view_defaults
 
 from .models import (
     DBSession,
     Guess,
     )
 
+@view_defaults(route_name='enter_guess')
+class GuessViews:
+    def __init__(self, request):
+        self.request = request
 
-@view_config(route_name='home', renderer='templates/new.jinja2')
-def my_view(request):
-    return {'project': 'BabySweepstakes'}
+    @view_config(renderer='templates/new.jinja2')
+    def new_guess(self):
+        return {}
 
+    @view_config(request_method='POST')
+    def save_guess(self):
+        your_name = self.request.params['your_name']
+        baby_sex = self.request.params['baby_sex']
+        days_late = self.request.params['days_late']
+        pounds = int(self.request.params['pounds'])
+        just_ounces = int(self.request.params['ounces'])
+        all_in_ounces = pounds * 16 + just_ounces
+        guess = Guess(your_name=your_name, baby_sex=baby_sex, days_late=days_late, ounces=all_in_ounces)
+        DBSession.add(guess)
+        return HTTPFound(location=self.request.route_url('show_guesses'))
 
-@view_config(route_name='new_guess')
-def new_guess(request):
-    your_name = request.params['your_name']
-    baby_sex = request.params['baby_sex']
-    days_late = request.params['days_late']
-    pounds = int(request.params['pounds'])
-    just_ounces = int(request.params['ounces'])
-    all_in_ounces = pounds * 16 + just_ounces
-    guess = Guess(your_name=your_name, baby_sex=baby_sex, days_late=days_late, ounces=all_in_ounces)
-    DBSession.add(guess)
-    return HTTPFound(location=request.route_url('guesses'))
-
-
-@view_config(route_name='guesses', renderer='templates/guesses.jinja2')
-def guesses(request):
-    return {'guesses': DBSession.query(Guess).order_by(Guess.id).all()}
+    @view_config(route_name='show_guesses', renderer='templates/guesses.jinja2')
+    def guesses(self):
+        return {'guesses': DBSession.query(Guess).order_by(Guess.id).all()}
